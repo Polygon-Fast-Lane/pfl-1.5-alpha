e4d01be03fc4bab9e6c927420d3c02f87007e8db
Alex <dalexwatts@gmail.com>
2022-11-07 01:52:57 -0800

    updated FastLaneAuctionRelay smart contract - simplified the interfacing. updated the searcher example contract to match

--- pfl_data/pfl-auction-beta.sol
+++ pfl_data/pfl-auction-beta.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.16;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 struct Round {
     uint24 roundNumber;
@@ -11,6 +12,10 @@ struct Round {
     uint64 endBlock;
     uint24 nextValidatorIndex;
     bool completedPayments;
+}
+
+interface ISearcherContract {
+    function fastLaneCall(bytes calldata, uint256, address) external payable returns (bool, bytes memory);
 }
 
 abstract contract FastLaneRelayEvents {
@@ -29,7 +34,7 @@ abstract contract FastLaneRelayEvents {
     error RelayInequalityTooHigh();
 
     error RelayPermissionPaused();
-    error RelayPermissionNotFastlaneValidator();
+    error RelayPermissionNotFastLaneValidator();
 
     error RelayWrongInit();
     error RelayWrongSpecifiedValidator();
@@ -51,106 +56,112 @@ abstract contract FastLaneRelayEvents {
     error ProcessingAmountExceedsBalance(uint256 amountRequested, uint256 balance);
 }
 
-contract FastLaneRelay is FastLaneRelayEvents, Ownable, ReentrancyGuard {
+contract FastLaneAuctionRelay is FastLaneRelayEvents, Ownable, ReentrancyGuard {
 
     uint24 internal currentRoundNumber;
     uint24 internal lastRoundProcessed;
-    uint24 public fastlaneStakeShare;
-
+    uint24 public fastLaneStakeShare;
     uint256 internal stakeSharePayable;
-
     bool public paused = false;
     bool internal isProcessingPayments = false;
-
-    bytes4 public FastLaneCallSelector = bytes4(keccak256(bytes('FastLaneCall')));
 
     mapping(address => bool) internal validatorsMap;
     mapping(uint24 => mapping(address => uint256)) internal validatorBalanceMap; // map[round][validator] = balance
     mapping(address => uint256) internal validatorBalancePayableMap;
     mapping(uint24 => Round) internal roundDataMap;
-    mapping(address => mapping(address => bool)) internal searcherContractEOAMap;
     mapping(bytes32 => uint256) internal fulfilledAuctionMap;
 
     address[] internal participatingValidators;
     address[] internal removedValidators; 
 
     constructor() {
-        if (address(this) == address(0)) revert RelayWrongInit();
+        if (address(this) == address(0)) revert("RelayWrongInit");
 
-        setFastlaneStakeShare(uint24(5_000));
+        setFastLaneStakeShare(uint24(5_000));
 
         currentRoundNumber = uint24(1);
-        roundDataMap[currentRoundNumber] = Round(currentRoundNumber, fastlaneStakeShare, uint64(block.number), 0, 0, false);
+        roundDataMap[currentRoundNumber] = Round(currentRoundNumber, fastLaneStakeShare, uint64(block.number), 0, 0, false);
 
         emit RelayInitialized(address(this));
     }
-    
+
     function submitFastLaneBid(
-        uint256 _bidAmount, // Value commited to be repaid at the end of execution
-        bytes32 _oppTxHash, // Target TX
-        address _searcherToAddress,
-        bytes calldata _toForwardExecData 
-        // _toForwardExecData should contain _bidAmount somewhere in the data to be decoded on the receiving searcher contract
-        ) external payable nonReentrant whenNotPaused onlyParticipatingValidators senderIsOrigin {
-
-            bytes32 auction_key = keccak256(abi.encode(_oppTxHash, tx.gasprice));
-            // NOTE: using abi.encodePacked may make this spoofable by clever antagonists 
-            // who shift decimals in certain (rare) scenarios
-
-            uint256 existing_bid = fulfilledAuctionMap[auction_key];
-
-            if (existing_bid != 0) {
-                if (_bidAmount >= existing_bid) {
-                    revert AuctionBidReceivedLate();
-                } else {
-                    revert AuctionSearcherNotWinner(_bidAmount, existing_bid);
-                }
-            }
-
-            if (!searcherContractEOAMap[_searcherToAddress][msg.sender]) {
-                revert AuctionEOANotEnabled();
-            }
-
-            if (_searcherToAddress == address(0) || _bidAmount == 0) revert RelaySearcherWrongParams();
+            uint256 _bidAmount, // Value commited to be repaid at the end of execution
+            bytes32 _oppTxHash, // Target TX
+            address _searcherToAddress,
+            bytes calldata _searcherCallData 
+        ) external payable nonReentrant whenNotPaused onlyParticipatingValidators senderIsOrigin returns (bytes memory) {
             
-            uint256 balanceBefore = address(this).balance - msg.value;
+            // make sure another searcher hasn't already won the opp,
+            // if so, revert early to save gas
+            checkBid(_oppTxHash, _bidAmount);
 
-            (bool success, bytes memory retData) = _searcherToAddress.call{value: msg.value}(
-                abi.encodePacked(
-                    FastLaneCallSelector, 
-                    _toForwardExecData, 
-                    msg.sender
-                )
+            // safely send any msg.value to the searcher's contract
+            forwardTxValue(_searcherToAddress);
+
+            // store the current balance, excluding msg.value
+            uint256 balanceBefore = address(this).balance;
+
+            // call the searcher's contract (see searcher_contract.sol for example of call receiver)
+            (bool success, bytes memory returnedData) = ISearcherContract(_searcherToAddress).fastLaneCall(
+                _searcherCallData, 
+                _bidAmount, 
+                msg.sender
             );
 
-            if (!success) revert RelaySearcherCallFailure(retData);
+            // if searcher's call failed, revert 
+            // (can probably remove this line, but I want to leave room for strange error handling on the searcher's end)
+            if (!success) revert("RelaySearcherCallFailure");
 
-            uint256 expected = balanceBefore + _bidAmount;
-            uint256 balanceAfter = address(this).balance;
-            if (balanceAfter < expected) revert RelayNotRepaid(expected - balanceAfter);
-
-            validatorBalanceMap[currentRoundNumber][block.coinbase] += _bidAmount;
-            fulfilledAuctionMap[auction_key] = _bidAmount;
-
+            // verify that the searcher paid the amount they bid
+            handleBalances(_bidAmount, balanceBefore);
             emit RelayFlashBid(msg.sender, _bidAmount, _oppTxHash, block.coinbase, _searcherToAddress);
+
+            // return the results of the searcher's call
+            return returnedData;
     }
 
-    function authorizeSearcherEOA(
-        address _searcherEOA
-    ) external {
-        // This is designed to be called by searchers' smart contracts
-        // therefore msg.sender should be the smart contract. 
-        searcherContractEOAMap[msg.sender][_searcherEOA] = true;
+    function checkBid(bytes32 _oppTxHash, uint256 _bidAmount) internal {
+        if (_bidAmount == 0) revert("RelaySearcherWrongParams");
+        
+        // Use hash of the opportunity tx hash and the transaction's gasprice as key for bid tracking
+        // This is dependent on the PFL Relay verifying that the searcher's gasprice matches
+        // the opportunity's gasprice, and that the searcher used the correct opportunity tx hash
+
+        bytes32 auction_key = keccak256(abi.encode(_oppTxHash, tx.gasprice));
+        // NOTE: using abi.encodePacked may make this spoofable by clever antagonists 
+        // who shift decimals in certain (rare) scenarios
+
+        uint256 existing_bid = fulfilledAuctionMap[auction_key];
+
+        if (existing_bid != 0) {
+            if (_bidAmount >= existing_bid) {
+                // TODO: This error message could also arise if the tx was sent via mempool
+                revert("AuctionBidReceivedLate");
+            } else {
+                revert(string(abi.encodePacked("AuctionSearcherNotWinner ", Strings.toString(_bidAmount), " ", Strings.toString(existing_bid))));
+            }
+        }
+
+        // Mark this auction as being complete to provide quicker reverts for subsequent searchers
+        fulfilledAuctionMap[auction_key] = _bidAmount;
     }
 
-    function deauthorizeSearcherEOA(
-        address _searcherEOA
-    ) external {
-        // This is designed to be called by searchers' smart contracts
-        // therefore msg.sender will be the smart contract. 
-        searcherContractEOAMap[msg.sender][_searcherEOA] = false;
+    function forwardTxValue(address _searcherToAddress) internal {
+        if (msg.value > 0) {
+            safeTransferETH(_searcherToAddress, msg.value);
+        }
     }
 
+    function handleBalances(uint256 _bidAmount, uint256 balanceBefore) internal {
+        uint256 expected = balanceBefore + _bidAmount;
+        uint256 balanceAfter = address(this).balance;
+        if (balanceAfter < expected) {
+            revert(string(abi.encodePacked("RelayNotRepaid ", Strings.toString(expected), " ", Strings.toString(balanceAfter))));
+        }
+
+        validatorBalanceMap[currentRoundNumber][block.coinbase] += _bidAmount;
+    }
 
     /// @notice Internal, calculates cuts
     /// @dev validatorCut 
@@ -181,7 +192,7 @@ contract FastLaneRelay is FastLaneRelayEvents, Ownable, ReentrancyGuard {
         roundDataMap[currentRoundNumber].endBlock = currentBlockNumber;
         currentRoundNumber++;
 
-        roundDataMap[currentRoundNumber] = Round(currentRoundNumber, fastlaneStakeShare, currentBlockNumber, 0, 0, false);
+        roundDataMap[currentRoundNumber] = Round(currentRoundNumber, fastLaneStakeShare, currentBlockNumber, 0, 0, false);
     }
 
     function processValidatorsBalances() external whenNotPaused senderIsOrigin returns (bool) {
@@ -189,9 +200,9 @@ contract FastLaneRelay is FastLaneRelayEvents, Ownable, ReentrancyGuard {
         // process rounds sequentially
         uint24 roundNumber = lastRoundProcessed + 1;
 
-        if (roundNumber >= currentRoundNumber) revert ProcessingRoundNotOver();
+        if (roundNumber >= currentRoundNumber) revert("ProcessingRoundNotOver");
 
-        if (roundDataMap[roundNumber].completedPayments) revert ProcessingRoundFullyPaidOut();
+        if (roundDataMap[roundNumber].completedPayments) revert("ProcessingRoundFullyPaidOut");
 
         isProcessingPayments = true;
 
@@ -256,31 +267,11 @@ contract FastLaneRelay is FastLaneRelayEvents, Ownable, ReentrancyGuard {
         }
     }
 
-    function getValidatorBalance(address validator) public view returns (uint256, uint256) {
-        // returns balancePayable, balancePending
-        if (isProcessingPayments) revert ProcessingInProgress();
-        uint256 balancePending; 
-        uint256 netValidatorRevenue;
-        for (uint24 _roundNumber = lastRoundProcessed + 1; _roundNumber <= currentRoundNumber; _roundNumber++) {
-            (netValidatorRevenue,) = _calculateStakeShare(validatorBalanceMap[_roundNumber][validator], roundDataMap[_roundNumber].stakeAllocation);
-            balancePending += netValidatorRevenue;
-        }
-        return (validatorBalancePayableMap[validator], balancePending);
-    }
-
-    function payValidator(address validator) public whenNotPaused senderIsOrigin returns (uint256) {
-        if (validatorBalancePayableMap[validator] == 0) revert ProcessingNoBalancePayable();
-        if (isProcessingPayments) revert ProcessingInProgress();
-        uint256 payableBalance = validatorBalancePayableMap[validator];
-        validatorBalancePayableMap[validator] = 0;
-        payable(validator).transfer(payableBalance);
-        emit ProcessingPaidValidator(validator, payableBalance);
-        return payableBalance;
-    }
-
     function withdrawStakeShare(address recipient, uint256 amount) external onlyOwner {
         // TODO: Add limitations around recipient & amount (integrate DAO controls / voting results)
-        if (amount > stakeSharePayable) revert ProcessingAmountExceedsBalance(amount, stakeSharePayable);
+        if (amount > stakeSharePayable) {
+            revert(string(abi.encodePacked("ProcessingAmountExceedsBalance ", Strings.toString(amount), " ", Strings.toString(stakeSharePayable))));
+        }
         stakeSharePayable -= amount;
         payable(recipient).transfer(amount);
         emit ProcessingWithdrewStakeShare(recipient, amount);
@@ -289,17 +280,16 @@ contract FastLaneRelay is FastLaneRelayEvents, Ownable, ReentrancyGuard {
     /// @notice Sets the stake revenue allocation (out of 1_000_000 (ie v2 fee decimals))
     /// @dev Initially set to 50_000 (5%) 
     /// Can't change the stake revenue allocation mid round - all changes go into effect in next round
-    /// @param _fastlaneStakeShare Protocol stake allocation on bids
-    function setFastlaneStakeShare(uint24 _fastlaneStakeShare)
+    /// @param _fastLaneStakeShare Protocol stake allocation on bids
+    function setFastLaneStakeShare(uint24 _fastLaneStakeShare)
         public
         onlyOwner
     {
-        if (_fastlaneStakeShare > 1000000) revert RelayInequalityTooHigh();
-        fastlaneStakeShare = _fastlaneStakeShare;
-        emit RelayShareSet(_fastlaneStakeShare);
+        if (_fastLaneStakeShare > 1_000_000) revert("RelayInequalityTooHigh");
+        fastLaneStakeShare = _fastLaneStakeShare;
+        emit RelayShareSet(_fastLaneStakeShare);
     }
     
-
     function enableRelayValidatorAddress(address _validator) external onlyOwner {
         if (!validatorsMap[_validator]) {
             // check to see if this validator is being re-added
@@ -346,7 +336,69 @@ contract FastLaneRelay is FastLaneRelayEvents, Ownable, ReentrancyGuard {
             validatorsMap[_validator] = false;
             emit RelayValidatorDisabled(_validator);
         }
-        
+    }
+
+    /***********************************|
+    |          Validator Functions      |
+    |__________________________________*/
+
+    function payValidator(address validator) public whenNotPaused senderIsOrigin returns (uint256) {
+        if (validatorBalancePayableMap[validator] == 0) revert("ProcessingNoBalancePayable");
+        if (isProcessingPayments) revert("ProcessingInProgress");
+        if (msg.sender != validator || msg.sender != owner()) revert("UnauthorizedPayor");
+        uint256 payableBalance = validatorBalancePayableMap[validator];
+        validatorBalancePayableMap[validator] = 0;
+        payable(validator).transfer(payableBalance);
+        emit ProcessingPaidValidator(validator, payableBalance);
+        return payableBalance;
+    }
+
+    function getValidatorBalance(address validator) public view returns (uint256, uint256) {
+        // returns balancePayable, balancePending
+        if (isProcessingPayments) revert("ProcessingInProgress");
+        uint256 balancePending; 
+        uint256 netValidatorRevenue;
+        for (uint24 _roundNumber = lastRoundProcessed + 1; _roundNumber <= currentRoundNumber; _roundNumber++) {
+            (netValidatorRevenue,) = _calculateStakeShare(validatorBalanceMap[_roundNumber][validator], roundDataMap[_roundNumber].stakeAllocation);
+            balancePending += netValidatorRevenue;
+        }
+        return (validatorBalancePayableMap[validator], balancePending);
+    }
+
+    /***********************************|
+    |        State View Functions       |
+    |__________________________________*/
+
+    function getCurrentRound() public view returns (uint24 _currentRoundNumber) {
+        _currentRoundNumber = currentRoundNumber;
+    }
+
+    function getLastRoundProcessed() public view returns (uint24 _lastRoundProcessed) {
+        _lastRoundProcessed = lastRoundProcessed;
+    }
+
+    function getFastLaneStakeShare() public view returns (uint24 _fastLaneStakeShare) {
+        _fastLaneStakeShare = fastLaneStakeShare;
+    }
+
+    function getPausedStatus() public view returns (bool _paused) {
+        _paused = paused;
+    } 
+
+
+    /***********************************|
+    |           Helper Functions        |
+    |__________________________________*/
+
+    function safeTransferETH(address to, uint256 amount) internal {
+        bool success;
+
+        assembly {
+            // Transfer the ETH and store if it succeeded or not.
+            success := call(gas(), to, amount, 0, 0, 0, 0)
+        }
+
+        require(success, "ETH_TRANSFER_FAILED");
     }
 
     /***********************************|
@@ -354,17 +406,17 @@ contract FastLaneRelay is FastLaneRelayEvents, Ownable, ReentrancyGuard {
     |__________________________________*/
 
     modifier whenNotPaused() {
-        if (paused) revert RelayPermissionPaused();
+        if (paused) revert("RelayPermissionPaused");
         _;
     }
 
     modifier senderIsOrigin() {
-        if (msg.sender != tx.origin) revert AuctionCallerMustBeSender();
+        if (msg.sender != tx.origin) revert("AuctionCallerMustBeSender");
         _;
     }
 
     modifier onlyParticipatingValidators() {
-        if (!validatorsMap[block.coinbase]) revert RelayPermissionNotFastlaneValidator();
+        if (!validatorsMap[block.coinbase]) revert("RelayPermissionNotFastLaneValidator");
         _;
     }
 }
--- searcher_contract.sol
+++ searcher_contract.sol
@@ -1,23 +1,21 @@
 pragma solidity ^0.8.16;
 //SPDX-License-Identifier: Unlicensed
 
-interface IFastLaneAuction {
-    function authorizeSearcherEOA(address) external;
-}
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract SearcherMinimalRawContract {
+contract FastLaneSearcherWrapper is ReentrancyGuard {
 
     address private owner;
     address payable private PFLAuction;
 
     error WrongPermissions();
     error OriginEOANotOwner();
-    error SearcherCallFailure(bytes retData);
-    error SearcherCallUnsuccessful();
-    error SearcherBadFuncSelector(bytes4 _selector);
+    error SearcherCallUnsuccessful(bytes retData);
+    error SearcherInsufficientFunds(uint256 amountToSend, uint256 currentBalance);
 
-    address public anAddress;
-    uint256 public anAmount;
+    address public anAddress; // just a var to change for the placeholder MEV function
+    uint256 public anAmount; // another var to change for the placeholder MEV function
 
     mapping(address => bool) internal approvedEOAs;
 
@@ -25,108 +23,35 @@ contract SearcherMinimalRawContract {
         owner = msg.sender;
     }
 
-    struct DoStuffCallArgs {
-        address anAddress;
-        uint256 anAmount;
-    }
-
-    // You choose your params as you want,
-    // You will declare them in the `submitBid` transaction to PFL
-    function FastLaneCall(bytes calldata _encodedCall) external payable onlyRelayer {
-
-        // In a relayed context _msgSender() will point back to the EOA that signed the searcherTX
-        // as the normal `msg.sender` points to the relayer.
-        // see https://ethereum.stackexchange.com/questions/99250/understanding-openzeppelins-context-contract
-        if (!approvedEOAs[_msgSender()]) revert OriginEOANotOwner();
-
-        /* 
-            ...
-            Do whatever you want here, call your usual searcher contract, use calldata, msg.data, etc...
-            ...
-        */
+    // the FastLane Auction contract will call this function
+    function fastLaneCall(
+        bytes calldata _searcherCallData, // contains func selector and calldata for your MEV transaction
+        uint256 _bidAmount,
+        address _sender
+    ) external payable onlyRelayer nonReentrant returns (bool, bytes memory) {
         
-        // MySearcherMEVContract.call(whatever); or
-        // Someopportunity.call(whatever)
-        // _target.call(_encodedCall);
+        // make sure it's your own EOA that's calling your contract 
+        checkFastLaneEOA(_sender);
+
+        // execute the searcher's intended function
+        (bool success, bytes memory returnedData) = address(this).call(_searcherCallData);
         
-        // or internally:
-        // NOTE: This method assumes you pay the FastLane Auction bid inside your function:
-        /*
-            (bool success, bytes memory retData) = address(this).call(
-                // remove last 20 bytes b/c of msg.sender address tacked on by FastLane Auction Contract
-                _encodedCall[:_encodedCall.length-20]
-            );
-            if (!success) revert SearcherCallFailure(retData);
-        */
+        // if the call didn't turn out the way you wanted, revert either here or inside your MEV function itself
+        if (!success) revert("SearcherCallUnsuccessful");
+
+        // balance check then Repay PFL at the end
+        if (address(this).balance < _bidAmount) {
+            revert(string(abi.encodePacked("SearcherInsufficientFunds  ", Strings.toString(_bidAmount), Strings.toString(address(this).balance))));
+        }
+        safeTransferETH(PFLAuction, _bidAmount);
         
-        // another internal version, if you want to handle the PFL payment here and use a function selector:
-        bytes4 _selector;
-        assembly {
-            _selector := calldataload(_encodedCall.offset)
-        }
-
-        if (_selector == this.doStuff.selector) {
-            // then decode the rest the args, making sure to remove the address of msg.sender at the end
-            DoStuffCallArgs memory funcArgs = abi.decode(
-                _encodedCall[4:_encodedCall.length-20], 
-                (DoStuffCallArgs)
-            );
-            
-            // do the stuff
-            bool isSuccessful = doStuff(funcArgs.anAddress, funcArgs.anAmount);
-
-            // if the stuff didn't turn out the way you wanted, revert here or inside your MEV function itself
-            if (!isSuccessful) revert SearcherCallUnsuccessful();
-
-            // Repay PFL at the end
-            safeTransferETH(PFLAuction, funcArgs.anAmount);
-
-        } else {
-            // revert if the selector can't find a match
-            revert SearcherBadFuncSelector(_selector);
-        }
+        // return the return data (optional)
+        return (success, returnedData);
     }
 
-    function doStuff(address _anAddress, uint256 _anAmount) public returns (bool) {
-        // your MEV function here.
-
-        if (!isTrustedForwarder(msg.sender)) {
-            // Since this is your regular MEV function, make sure not to run any 
-            // msg.sender safety checks that would preclude the FastLane Auction 
-            // from calling.
-            // TODO: find a better / safer way to handle ^ or use a separate func wrapper
-            // for non-FastLane calls and make this func internal-only.
-
-            if (!approvedEOAs[msg.sender]) revert OriginEOANotOwner();
-        }
-
-        // Do MEV stuff here
-        anAddress = _anAddress;
-        anAmount = _anAmount;
-        bool isSuccessful = true;
-        return isSuccessful;
-    }
-
-    function _msgSender() internal view returns (address sender) {
-        if (isTrustedForwarder(msg.sender)) {
-            // The assembly code is more direct than the Solidity version using `abi.decode`.
-            /// @solidity memory-safe-assembly
-            assembly {
-                sender := shr(96, calldataload(sub(calldatasize(), 20)))
-            }
-        } else {
-            return msg.sender;
-        }
-    }
-
-    // Can receive ETH
-    fallback() external payable {}
-    receive() external payable {}
-
-    function isTrustedForwarder(address forwarder) public view returns (bool) {
-        return forwarder == PFLAuction;
-    }
-
+    // Other functions / modifiers that are necessary for FastLane integration:
+    // NOTE: you can use your own versions of these, or find alternative ways
+    // to implement similar safety checks. Please be careful when altering!
     function safeTransferETH(address to, uint256 amount) internal {
         bool success;
 
@@ -139,18 +64,43 @@ contract SearcherMinimalRawContract {
     }
 
     function setPFLAuctionAddress(address _pflAuction) public {
-        if (msg.sender != owner) revert OriginEOANotOwner();
+        if (msg.sender != owner) revert("OriginEOANotOwner");
         PFLAuction = payable(_pflAuction);
     }
 
     function approveFastLaneEOA(address eoaAddress) public {
-        if (msg.sender != owner) revert OriginEOANotOwner();
-        IFastLaneAuction(PFLAuction).authorizeSearcherEOA(eoaAddress);
+        if (msg.sender != owner) revert("OriginEOANotOwner");
         approvedEOAs[eoaAddress] = true;
     }
 
+    function checkFastLaneEOA(address eoaAddress) view internal {
+        if (!approvedEOAs[eoaAddress]) revert("SenderEOANotApproved");
+    }
+
+    function isTrustedForwarder(address forwarder) public view returns (bool) {
+        return forwarder == PFLAuction;
+    }
+
     modifier onlyRelayer {
-          if (!isTrustedForwarder(msg.sender)) revert WrongPermissions();
+          if (!isTrustedForwarder(msg.sender)) revert("InvalidPermissions");
           _;
      }
+}
+
+contract SearcherContractExample is FastLaneSearcherWrapper {
+    // your own MEV function here 
+    // NOTE: its security checks must be compatible w/ calls from the FastLane Auction Contract
+    function doStuff(address _anAddress, uint256 _anAmount) public payable returns (bool) {
+        // NOTE: this function can't be external as the FastLaneCall func will call it internally
+
+        if (!isTrustedForwarder(msg.sender)) { // example of safety check modification - only check msg.sender when not forwarded
+            if (!approvedEOAs[msg.sender]) revert("SenderEOANotApproved");
+        }
+
+        // Do MEV stuff here
+        anAddress = _anAddress;
+        anAmount = _anAmount;
+        bool isSuccessful = true;
+        return isSuccessful;
+    }
 }
